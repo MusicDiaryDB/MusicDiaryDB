e615591de9ebce84ba6192116ac9bdb43690a27e
ptdewey <patrickdewey2002@gmail.com>
2024-10-11 18:38:02 -0400

    feat: working modular endpoints, 8 main total working endpoints

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#
+# NOTE: This file is a WIP, but should be easier to work with than the original main.py
+# - it may or may not work correctly currently
+#
+
 from flask import Flask, request, jsonify, render_template
 import psycopg2
 from psycopg2.extras import RealDictCursor
@@ -30,263 +35,192 @@ def execute_query(query, params=None, fetch_one=False):
     try:
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute(query, params)
-            # If SELECT statement, fetch results
             if query.strip().upper().startswith("SELECT"):
-                if fetch_one:
-                    result = cur.fetchone()
-                else:
-                    result = cur.fetchall()
+                result = cur.fetchone() if fetch_one else cur.fetchall()
             else:
-                # For INSERT, UPDATE, DELETE return the row count
                 result = cur.rowcount
             conn.commit()
     except Exception as e:
         print(f"Error: {e}")
         conn.rollback()
     finally:
-        if conn:  # Ensure the connection is closed only if it was created
+        if conn:
             conn.close()
     return result
+
+
+def create_resource(table, params, primary_key):
+    keys = ', '.join(
+        [f'"{key[0].upper() + key[1:].replace("Id", "ID")}"' for key in params.keys()])
+    values = ', '.join(['%s' for _ in params.keys()])
+    query = f'INSERT INTO "{table}" ({keys}) VALUES ({values}) RETURNING "{
+        primary_key}"'
+    return execute_query(query, tuple(params.values()), fetch_one=True)
+
+
+def update_resource(table, params, identifier, primary_key):
+    set_clause = ', '.join(
+        [f'"{key[0].upper() + key[1:].replace("Id", "ID")}" = %s' for key in params.keys()])
+    query = f'UPDATE "{table}" SET {set_clause} WHERE "{primary_key}" = %s'
+    return execute_query(query, tuple(params.values()) + (identifier,))
+
+
+def delete_resource(table, identifier, primary_key):
+    query = f'DELETE FROM "{table}" WHERE "{primary_key}" = %s'
+    return execute_query(query, (identifier,))
+
+
+def get_resource(table, identifier, primary_key):
+    query = f'SELECT * FROM "{table}" WHERE "{primary_key}" = %s'
+    return execute_query(query, (identifier,), fetch_one=True)
+
+
+def handle_request(table, operation, required_fields, primary_key, identifier=None):
+    # Extract and validate parameters
+    data = request.form
+    params = {field: data.get(field) for field in required_fields}
+
+    if any(value is None for value in params.values()):
+        return jsonify({"error": f"Missing fields: {params.values()}"}), 400
+
+    # Perform the requested operation
+    if operation.lower() == 'create':
+        resource_id = create_resource(table, params, primary_key)
+        if resource_id is None:
+            return jsonify({"error": f"{table} creation failed"}), 500
+        return jsonify({"message": f"{table} created", f"{primary_key}": resource_id}), 201
+
+    elif operation.lower() == 'update':
+        result = update_resource(table, params, identifier, primary_key)
+        if result is None or result == 0:
+            return jsonify({"error": f"{table} update failed"}), 404
+        return jsonify({"message": f"{table} updated"}), 200
+
+    elif operation.lower() == 'delete':
+        result = delete_resource(table, identifier, primary_key)
+        if result is None or result == 0:
+            return jsonify({"error": f"{table} not found"}), 404
+        return jsonify({"message": f"{table} deleted"}), 200
+
 
 # ============================
 #         USER ROUTES
 # ============================
 
-
-# POST /user/ - Create a new user
 @app.route('/user/', methods=['POST'])
 def create_user():
-    data = request.form
-    username = data.get('username')
-    visibility = data.get('visibility')
-
-    if not username or not visibility:
-        return jsonify({"error": "Missing fields"}), 400
-
-    existing_user_query = 'SELECT * FROM "User" WHERE "Username" = %s'
-    existing_user = execute_query(
-        existing_user_query, (username,), fetch_one=True)
-
-    if existing_user:
-        return jsonify({"error": "Username already exists"}), 400
-
-    query = """
-        INSERT INTO "User" ("Username", "Visibility")
-        VALUES (%s, %s) RETURNING "UserID"
-    """
-
-    user_id = execute_query(query, (username, visibility), fetch_one=True)
-
-    if user_id is None:
-        return jsonify({"error": "User creation failed"}), 500
-
-    return jsonify({"message": "User created", "UserID": user_id}), 201
+    return handle_request('User', 'create', ['username', 'visibility'], 'UserID')
 
 
-# DELETE /user/:userId - Delete user by UserID
-@app.route('/user/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    print(f"Deleting user with ID: {user_id}")
-
-    query = 'DELETE FROM "User" WHERE "UserID" = %s'
-    result = execute_query(query, (user_id,))
-
-    if result is None or result == 0:
-        return jsonify({"error": "User not found"}), 404
-
-    return jsonify({"message": "User deleted"}), 200
-
-
-# PUT /user/:userId - Update user
-@app.route('/user/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    data = request.form
-    username = data.get('username')
-    visibility = data.get('visibility')
-
-    if not username or not visibility:
-        return jsonify({"error": "Missing fields"}), 400
-
-    # Check if the user exists before updating
-    existing_user_query = 'SELECT * FROM "User" WHERE "UserID" = %s'
-    existing_user = execute_query(
-        existing_user_query, (user_id,), fetch_one=True)
-
-    if not existing_user:
-        return jsonify({"error": "User not found"}), 404
-
-    query = '''
-        UPDATE "User"
-        SET "Username" = %s, "Visibility" = %s
-        WHERE "UserID" = %s
-    '''
-    result = execute_query(query, (username, visibility, user_id))
-
-    # Check if any rows were updated
-    if result is None or result == 0:  # No rows affected
-        return jsonify({"error": "User update failed"}), 500
-
-    return jsonify({"message": "User updated"}), 200
-
-
-# GET /user/:userId - Get user by UserID
 @app.route('/user/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    query = 'SELECT * FROM "User" WHERE "UserID" = %s'
-    user = execute_query(query, (user_id,), fetch_one=True)
-
-    if not user:
-        return jsonify({"error": "User not found"}), 404
-
-    return jsonify(user), 200
+    return handle_request('User', 'get', [], 'UserID', user_id)
 
 
-# ============================
-#     DIARY ENTRY ROUTES
-# ============================
-
-# POST /entry/ - Create a new diary entry
-@app.route('/entry/', methods=['POST'])
-def create_diary_entry():
-    data = request.form
-    date = data.get('date')  # Get the date from the form data
-    description = data.get('description')  # Get the description
-    visibility = data.get('visibility')  # Get the visibility
-    user_id = data.get('userId')  # Get the user ID
-
-    # Validate required fields
-    if not date or not visibility or not user_id:
-        return jsonify({"error": "Missing fields"}), 400
-
-    # Check if a diary entry already exists for this date and user
-    existing_entry_query = '''
-        SELECT * FROM "DiaryEntry" WHERE "Date" = %s AND "UserID" = %s
-    '''
-    existing_entry = execute_query(
-        existing_entry_query, (date, user_id), fetch_one=True)
-
-    if existing_entry:
-        return jsonify({"error": "Diary entry for this date already exists"}), 400
-
-    query = '''
-        INSERT INTO "DiaryEntry" ("Date", "Description", "Visibility", "UserID")
-        VALUES (%s, %s, %s, %s) RETURNING "EntryID"
-    '''
-    entry_id = execute_query(
-        query, (date, description, visibility, user_id), fetch_one=True)
-
-    if entry_id is None:
-        return jsonify({"error": "Diary entry creation failed"}), 500
-
-    return jsonify({"message": "Diary entry created", "EntryID": entry_id}), 201
+@app.route('/user/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    return handle_request('User', 'update', ['username', 'visibility'], 'UserID', user_id)
 
 
-# PUT /entry/<int:entry_id> - Update a diary entry
-@app.route('/entry/<int:entry_id>', methods=['PUT'])
-def update_diary_entry(entry_id):
-    data = request.form
-    description = data.get('description')  # Get the description
-    visibility = data.get('visibility')  # Get the visibility
-
-    # Validate required fields
-    if not visibility:
-        return jsonify({"error": "Missing fields"}), 400
-
-    # Update query
-    query = '''
-        UPDATE "DiaryEntry"
-        SET "Description" = %s, "Visibility" = %s
-        WHERE "EntryID" = %s
-    '''
-    result = execute_query(query, (description, visibility, entry_id))
-
-    if result is None or result == 0:
-        return jsonify({"error": "Diary entry not found"}), 404
-
-    return jsonify({"message": "Diary entry updated", "EntryID": entry_id}), 200
-
-
-# DELETE /entry/:entryId - Delete diary entry
-@app.route('/entry/<int:entry_id>', methods=['DELETE'])
-def delete_diary_entry(entry_id):
-    query = 'DELETE FROM "DiaryEntry" WHERE "EntryID" = %s'
-
-    res = execute_query(query, (entry_id,))
-    if not res:
-        return jsonify({"error": f"Entry {entry_id} not found"}), 404
-
-    return jsonify({"message": "Diary entry deleted"}), 200
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    return handle_request('User', 'delete', [], 'UserID', user_id)
 
 
 # ============================
-#      DIARY REPORT ROUTES
+#     SONG ROUTES
 # ============================
 
-@app.route('/report/', methods=['POST'])
-def create_diary_report():
-    data = request.form
-    date = data.get('date')
-    description = data.get('description')
-    visibility = data.get('visibility')
-    user_id = data.get('userId')
-
-    if not date or not visibility or not user_id:
-        return jsonify({"error": "Missing fields"}), 400
-
-    existing_entry_query = '''
-        SELECT * FROM "DiaryReport" WHERE "Date" = %s AND "UserID" = %s
-    '''
-    existing_entry = execute_query(
-        existing_entry_query, (date, user_id), fetch_one=True)
-
-    if existing_entry:
-        return jsonify({"error": "Diary report for this date already exists"}), 400
-
-    query = '''
-        INSERT INTO "DiaryReport" ("Date", "Description", "Visibility", "UserID")
-        VALUES (%s, %s, %s, %s) RETURNING "ReportID"
-    '''
-    report_id = execute_query(
-        query, (date, description, visibility, user_id), fetch_one=True)
-
-    if report_id is None:
-        return jsonify({"error": "Diary report creation failed"}), 500
-
-    return jsonify({"message": "Diary report created", "ReportId": report_id}), 201
+@app.route('/song/', methods=['POST'])
+def create_song():
+    return handle_request('Song', 'create', ['name', 'releaseDate'], 'SongID')
 
 
-@app.route('/report/<int:report_id>', methods=['PUT'])
-def update_diary_report(report_id):
-    data = request.form
-    description = data.get('description')
-    visibility = data.get('visibility')
-
-    if not visibility:
-        return jsonify({"error": "Missing fields"}), 400
-
-    query = '''
-        UPDATE "DiaryReport"
-        SET "Description" = %s, "Visibility" = %s
-        WHERE "ReportID" = %s
-    '''
-    result = execute_query(query, (description, visibility, report_id))
-
-    if result is None or result == 0:
-        return jsonify({"error": "Diary report not found"}), 404
-
-    return jsonify({"message": "Diary report updated", "ReportID": report_id}), 200
+@app.route('/song/<int:song_id>', methods=['GET'])
+def get_song(song_id):
+    return handle_request('Song', 'get', [], 'SongID', song_id)
 
 
-# DELETE /report/:reportId - Delete a report
-@app.route('/report/<int:report_id>', methods=['DELETE'])
-def delete_report(report_id):
-    query = 'DELETE FROM "DiaryReport" WHERE "ReportID" = %s'
-    result = execute_query(query, (report_id,))
+@app.route('/song/<int:song_id>', methods=['PUT'])
+def update_song(song_id):
+    return handle_request('Song', 'update', ['name', 'releaseDate'], 'SongID', song_id)
 
-    if not result:
-        return jsonify({"error": "Report not found"}), 404
 
-    return jsonify({"message": "Report deleted"}), 200
+@app.route('/song/<int:song_id>', methods=['DELETE'])
+def delete_song(song_id):
+    return handle_request('Song', 'delete', [], 'SongID', song_id)
+
+
+# ============================
+#     ALBUM ROUTES
+# ============================
+
+@app.route('/album/', methods=['POST'])
+def create_album():
+    return handle_request('Album', 'create', ['name'], 'AlbumID')
+
+
+@app.route('/album/<int:album_id>', methods=['GET'])
+def get_album(album_id):
+    return handle_request('Album', 'get', [], 'AlbumID', album_id)
+
+
+@app.route('/album/<int:album_id>', methods=['PUT'])
+def update_album(album_id):
+    return handle_request('Album', 'update', ['name'], 'AlbumID', album_id)
+
+
+@app.route('/album/<int:album_id>', methods=['DELETE'])
+def delete_album(album_id):
+    return handle_request('Album', 'delete', [], 'AlbumID', album_id)
+
+
+# ============================
+#     ARTIST ROUTES
+# ============================
+
+@app.route('/artist/', methods=['POST'])
+def create_artist():
+    return handle_request('Artist', 'create', ['name'], 'ArtistID')
+
+
+@app.route('/artist/<int:artist_id>', methods=['GET'])
+def get_artist(artist_id):
+    return handle_request('Artist', 'get', [], 'ArtistID', artist_id)
+
+
+@app.route('/artist/<int:artist_id>', methods=['PUT'])
+def update_artist(artist_id):
+    return handle_request('Artist', 'update', ['name'], 'ArtistID', artist_id)
+
+
+@app.route('/artist/<int:artist_id>', methods=['DELETE'])
+def delete_artist(artist_id):
+    return handle_request('Artist', 'delete', [], 'ArtistID', artist_id)
+
+
+# ============================
+#     PLATFORM ROUTES
+# ============================
+
+@app.route('/platform/', methods=['POST'])
+def create_platform():
+    return handle_request('StreamingPlatform', 'create', ['name'], 'StreamingPlatformID')
+
+
+@app.route('/platform/<int:platform_id>', methods=['GET'])
+def get_platform(platform_id):
+    return handle_request('StreamingPlatform', 'get', [], 'StreamingPlatformID', platform_id)
+
+
+@app.route('/platform/<int:platform_id>', methods=['PUT'])
+def update_platform(platform_id):
+    return handle_request('StreamingPlatform', 'update', ['name'], 'StreamingPlatformID', platform_id)
+
+
+@app.route('/platform/<int:platform_id>', methods=['DELETE'])
+def delete_platform(platform_id):
+    return handle_request('StreamingPlatform', 'delete', [], 'StreamingPlatformID', platform_id)
 
 
 # ============================
@@ -296,63 +230,21 @@ def delete_report(report_id):
 # POST /review/:songId - Create a review for a song
 @app.route('/review/<int:song_id>', methods=['POST'])
 def create_review(song_id):
-    data = request.form
-    contents = data.get('contents')
-    visibility = data.get('visibility')
-
-    if not contents or not visibility:
-        return jsonify({"error": "Missing fields"}), 400
-
-    existing_review_query = 'SELECT * FROM "Review" WHERE "SongID" = %s'
-    existing_user = execute_query(
-        existing_review_query, (song_id,), fetch_one=True)
-
-    if existing_user:
-        return jsonify({"error": "Review for song already exists"}), 400
-
-    query = '''
-        INSERT INTO "Review" ("Contents", "Visibility", "SongID")
-        VALUES (%s, %s, %s) RETURNING "ReviewID"
-    '''
-    review_id = execute_query(
-        query, (contents, visibility, song_id), fetch_one=True)
-
-    return jsonify({"message": "Review created", "ReviewID": review_id}), 201
+    data = request.form.to_dict()
+    data['songId'] = song_id
+    return handle_request('Review', 'create', ['contents', 'visibility', 'songId'], 'ReviewID')
 
 
+# PUT /review/:reviewId - Update a review by its ID
 @app.route('/review/<int:review_id>', methods=['PUT'])
 def update_review(review_id):
-    data = request.form
-    contents = data.get('contents')  # Get the description
-    visibility = data.get('visibility')  # Get the visibility
-
-    # Validate required fields
-    if not visibility or not contents:
-        return jsonify({"error": "Missing fields"}), 400
-
-    # Update query
-    query = '''
-        UPDATE "Review"
-        SET "Contents" = %s, "Visibility" = %s
-        WHERE "ReviewID" = %s
-    '''
-    result = execute_query(query, (contents, visibility, review_id))
-
-    if result is None or result == 0:
-        return jsonify({"error": "Diary entry not found"}), 404
-
-    return jsonify({"message": "Diary entry updated", "ReviewId": review_id}), 200
+    return handle_request('Review', 'update', ['contents', 'visibility'], 'ReviewID', review_id)
 
 
+# DELETE /review/:reviewId - Delete a review by its ID
 @app.route('/review/<int:review_id>', methods=['DELETE'])
 def delete_review(review_id):
-    query = 'DELETE FROM "Review" WHERE "ReviewID" = %s'
-    existing_review = execute_query(query, (review_id,))
-
-    if not existing_review:
-        return jsonify({"error": f"Review {review_id} not found"}), 404
-
-    return jsonify({"message": "Review deleted"}), 200
+    return handle_request('Review', 'delete', [], 'ReviewID', review_id)
 
 
 @app.route('/')
@@ -361,5 +253,4 @@ def index():
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
