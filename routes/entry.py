0eecd4d7eaed6203bdb007ccbbc7095a68bd10be
ptdewey <patrickdewey2002@gmail.com>
2024-11-17 16:44:10 -0500

    feat: all entry and nice output entry routes

--- routes/entry.py
+++ routes/entry.py
@@ -45,12 +45,13 @@ def get_user_diary_entries(user_id: int) -> Any:
     query = """SELECT * FROM "DiaryEntry" WHERE "UserID" = '%s';"""
     return execute_query_ret_result(query, (user_id,))
 
+
 @bp.route("/entry/friends/<int:user_id>", methods=["GET"])
 def get_friends_diary_entries(user_id: int) -> Any:
     # Query to get the friends of the user
     friends_query = """SELECT "FriendID" FROM "UserFriends" WHERE "UserID" = '%s';"""
     friends_result = execute_query_ret_result(friends_query, (user_id,))
-    
+
     if not friends_result:
         return {"message": "No friends found for this user."}, 404
 
@@ -64,6 +65,40 @@ def get_friends_diary_entries(user_id: int) -> Any:
     AND "Visibility" IN ('Public', 'Friends');
     """
     formatted_friend_ids = ",".join([str(friend_id) for friend_id in friend_ids])
-    friends_entries_result = execute_query_ret_result(friends_entries_query % formatted_friend_ids, ())
+    friends_entries_result = execute_query_ret_result(
+        friends_entries_query % formatted_friend_ids, ()
+    )
     print(friends_entries_result)
     return friends_entries_result
+
+
+@bp.route("/entries/user/<int:user_id>", methods=["GET"])
+def get_user_entries_song(user_id) -> Any:
+    query = """
+    SELECT
+        de."EntryID",
+        de."Date",
+        de."Description",
+        de."Visibility",
+        de."UserID",
+        s."Name" AS "SongName",
+        al."Name" AS "AlbumName",
+        ar."Name" AS "ArtistName"
+    FROM
+        "DiaryEntry" de
+    INNER JOIN
+        "Song" s ON de."SongID" = s."SongID"
+    INNER JOIN
+        "Album" al ON s."AlbumID" = al."AlbumID"
+    INNER JOIN
+        "Artist" ar ON al."ArtistID" = ar."ArtistID"
+    WHERE
+        de."UserID" = %s
+    """
+    return execute_query_ret_result(query, (user_id,))
+
+
+@bp.route("/entries/all", methods=["GET"])
+def ge_all_entries() -> Any:
+    query = """SELECT * FROM "DiaryEntry"; """
+    return execute_query_ret_result(query)
